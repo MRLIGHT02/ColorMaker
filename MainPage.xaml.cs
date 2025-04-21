93ed69792c3e82f5598d8b9c2da6511981cbe059
Saif Ali <saifali42020@gmail.com>
2025-04-21 16:47:24 +0530

    added bool variable

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,14 +10,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	bool isRandom;
     private void sld_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-		var red = sldRed.Value;
-		var green = slGreen.Value;
-		var blue = sldBlue.Value;
+		if (!isRandom)
+			{
+            var red = sldRed.Value;
+            var green = slGreen.Value;
+            var blue = sldBlue.Value;
 
-		Color color = Color.FromRgb(red, green, blue);
-		SetColor(color);
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+            }
         }
 
     private void SetColor(Color color)
@@ -25,6 +29,21 @@ public partial class MainPage : ContentPage
 		btnRandom.Background = color;
 		Container.BackgroundColor = color;
 		lblHex.Text = color.ToHex();
+        }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+		isRandom = true;
+		var random = new Random();
+		var color = Color.FromRgb(
+			random.Next(0, 255),
+            random.Next(0, 255),
+            random.Next(0, 255));
+		SetColor(color);
+		sldRed.Value = color.Red;
+		slGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+		isRandom = false;
         }
     }
 
