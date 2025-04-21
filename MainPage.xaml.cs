ce1dbdf768a6fc19c893ff1c5470e2e49d0ba014
Saif Ali <saifali42020@gmail.com>
2025-04-21 16:19:08 +0530

    add fuction

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace ColorMaker;
+﻿
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
@@ -9,6 +10,21 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	
-}
+    private void sld_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+		var red = sldRed.Value;
+		var green = slGreen.Value;
+		var blue = sldBlue.Value;
+
+		Color color = Color.FromRgb(red, green, blue);
+		SetColor(color);
+        }
+
+    private void SetColor(Color color)
+        {
+		btnRandom.Background = color;
+		Container.BackgroundColor = color;
+		lblHex.Text = color.ToHex();
+        }
+    }
 
